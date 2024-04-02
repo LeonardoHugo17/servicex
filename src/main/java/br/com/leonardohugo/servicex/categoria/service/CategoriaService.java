1ba92f71a1fed1d6a7a90785e9543c37ab8f1594
leonardo <leonardolhfm70@gmail.com>
2024-04-01 21:50:57 -0300

    Commit Ordem Servico incompleto

--- src/main/java/br/com/leonardohugo/servicex/categoria/service/CategoriaService.java
+++ src/main/java/br/com/leonardohugo/servicex/categoria/service/CategoriaService.java
@@ -2,7 +2,6 @@ package br.com.leonardohugo.servicex.categoria.service;
 
 import br.com.leonardohugo.servicex.categoria.domain.Categoria;
 import br.com.leonardohugo.servicex.categoria.repository.CategoriaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
